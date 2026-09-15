6dbd144f4941a3ab4888f667c83cae3ca3b93d99
hushpark <parkdemon@gmail.com>
2026-09-15 14:58:02 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Electron 빌드 시 흰 화면 방지용 상대 경로 설정
-  server: {
-    host: true, // 교내 랜선/Wi-Fi IP 자동 개방
-    port: 5173,
-    allowedHosts: true, // 👈 ✨ 외부 Cloudflare 호스트 접속 차단(blocked request) 해제!
-  },
-});
+})
